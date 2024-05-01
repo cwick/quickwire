f1f73bd2c487410790cb411eab20292d96033858
Carmen Wick <carmen.wick@hey.com>
2024-05-01 00:14:52 -0700

    Remove the bootloader for now

--- src/platform/deno/cli.ts
+++ src/platform/deno/cli.ts
@@ -32,32 +32,33 @@ async function main(argv: string[]) {
   const isBootstrap = argv[0] === "__BOOTSTRAP__";
   argv = isBootstrap ? argv.slice(1) : argv;
 
-  const importPath = join(projectDir, "deno.json");
-  console.log("importPath", importPath);
-  const config = (
-    await import(importPath, {
-      with: { type: "json" },
-    })
-  ).default;
+  // const importPath = join(projectDir, "deno.json");
+  // console.log("importPath", importPath);
+  // const config = (
+  //   await import(importPath, {
+  //     with: { type: "json" },
+  //   })
+  // ).default;
 
-  const scriptName = `${config.imports["@quickwire/core"]}/cli.ts`;
-  const command = parseArgs(argv);
+  // const scriptName = `${config.imports["@quickwire/core"]}/cli.ts`;
+  // const command = parseArgs(argv);
 
-  if (isBootstrap) {
-    new Deno.Command(Deno.execPath(), {
-      args: [
-        "run",
-        ...(command === "start" ? ["--watch"] : []),
-        "--allow-read",
-        "--allow-write",
-        "--allow-net",
-        scriptName,
-        ...argv,
-      ],
-    }).spawn();
-  } else {
-    runCli(argv, platform);
-  }
+  // if (isBootstrap) {
+  //   new Deno.Command(Deno.execPath(), {
+  //     args: [
+  //       "run",
+  //       ...(command === "start" ? ["--watch"] : []),
+  //       "--allow-read",
+  //       "--allow-write",
+  //       "--allow-net",
+  //       import.meta.url,
+  //       ...argv,
+  //     ],
+  //   }).spawn();
+  // } else {
+  //   runCli(argv, platform);
+  // }
+  runCli(argv, platform);
 }
 
 if (import.meta.main) {
