f839b2d425a9dcf6cad26ab836e5b2372dae9456
Carmen Wick <carmen.wick@hey.com>
2024-05-03 15:59:32 -0700

    Clean up a few things

--- TODO.md
+++ TODO.md
@@ -1,15 +1,11 @@
-- runtime
-  - core
-  - platform
-- compiler
-  - bundle and export to web, desktop, and mobile
-- dev tools
+- export to different targets: web, static web, standalone, desktop etc...
 
-  - Project generator
+- qw init
 
-  - transpile TS on the fly with [deno_emit](https://github.com/denoland/deno_emit)
-    - Need to do this when qw is compiled, since dynamic imports don't work.
-    - Generate source map. Precompile JSX. See CompilerOptions
+- transpile TS on the fly with [deno_emit](https://github.com/denoland/deno_emit)
+
+  - Need to do this when qw is compiled, since dynamic imports don't work.
+  - Generate source map. Precompile JSX. See CompilerOptions
 
 - Distribution methods
 
@@ -19,6 +15,13 @@
   - Rails: Bootstrap script locates and runs the real script
 
 - SSL certs for dev
+
   - Autogenerate
   - [node-forge](https://github.com/digitalbazaar/forge) x509 certs
     - Look at [minica](https://github.com/jsha/minica/blob/master/main.go)
+
+- Code generation with [ts-morph](https://ts-morph.com/)
+
+- Fix hard coded temp directory in deno.json: --watch-exclude=/tmp
+
+- Example weather app
--- src/core/qw.ts
+++ src/core/qw.ts
@@ -4,38 +4,36 @@ import Server from "./server.ts";
 export type Command = "start" | "version" | "help" | "export";
 
 export default class QW {
-  #platform: Platform;
-
-  constructor(platform: Platform) {
-    this.#platform = platform;
+  constructor(private platform: Platform) {
+    this.platform = platform;
   }
 
   async run(command: Command) {
     if (command === "start") {
-      const server = new Server(this.#platform);
-      this.#platform.serve(server.handleRequest.bind(server));
+      const server = new Server(this.platform);
+      this.platform.serve(server.handleRequest.bind(server));
     } else if (command === "export") {
-      const Index = (await this.#platform.import("routes/index.tsx"))
+      const Index = (await this.platform.import("routes/index.tsx"))
         .default! as () => string;
-      await this.#platform.writeTextFile("index.html", Index());
-      this.#platform.exit(0);
+      await this.platform.writeTextFile("index.html", Index());
+      this.platform.exit(0);
     } else if (command === "version") {
-      this.#platform.log(`qw (quickwire) ${this.#platform.version}`);
-      this.#platform.exit(0);
+      this.platform.log(`qw (quickwire) ${this.platform.version}`);
+      this.platform.exit(0);
     } else {
-      this.#platform.log("Usage: qw [options] [COMMAND]");
-      this.#platform.log("");
-      this.#platform.log("Commands:");
-      this.#platform.log(
+      this.platform.log("Usage: qw [options] [COMMAND]");
+      this.platform.log("");
+      this.platform.log("Commands:");
+      this.platform.log(
         "  init\tCreate a new Quickwire project (not implemented)"
       );
-      this.#platform.log("  export\tExport the current project");
-      this.#platform.log("  start\tStart the current project");
-      this.#platform.log("");
-      this.#platform.log("Options:");
-      this.#platform.log("  --help     Show this message and exit.");
-      this.#platform.log("  --version  Print version information and exit.");
-      this.#platform.exit(0);
+      this.platform.log("  export\tExport the current project");
+      this.platform.log("  start\tStart the current project");
+      this.platform.log("");
+      this.platform.log("Options:");
+      this.platform.log("  --help     Show this message and exit.");
+      this.platform.log("  --version  Print version information and exit.");
+      this.platform.exit(0);
     }
   }
 }
